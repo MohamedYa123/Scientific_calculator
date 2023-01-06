942839cbb13b50a0653e06435f439eb65bf27d61
MohamedYa123 <tecyouth123@gmail.com>
2023-01-07 01:08:34 +0200

    first rock

---
+++ calculator/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        private static Dictionary<string,decimal> values = new Dictionary<string,decimal>();
+        /// <summary>
+        /// pass the math problem to evaluate
+        /// it can include
+        /// +-*/^ () symbols
+        /// sin(,cos(,tan(,sin-1(,cos-1(,tan-1(,abs,ln(,log10( functions
+        /// </summary>
+        /// <param name="eq"></param>
+        /// <returns></returns>
+        public static decimal Eval(string eq)
+        {
+            smartmodifier sm=new smartmodifier();
+            operation op = new operation(eq, sm,values);
+            return op.calc();
+        }
+    }
+}
